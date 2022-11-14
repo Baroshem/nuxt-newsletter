030c2148efb2de75a17c5eff094166c5681b78c7
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-11-14 14:12:11 +0100

    chore: bump nuxt to rc.13

--- src/runtime/server/api/revue.ts
+++ src/runtime/server/api/revue.ts
@@ -1,8 +1,8 @@
-import { useBody } from 'h3'
+import { defineEventHandler, createError, readBody } from 'h3'
 import { useRuntimeConfig } from '#imports'
 
-export default async (req, res) => {
-  const { email } = await useBody(req)
+export default defineEventHandler(async (event) => {
+  const { email } = await readBody(event)
 
   if (!email) {
     console.error('`[@nuxtjs/newsletter]` Missing `email` in the subscribe body')
@@ -12,7 +12,6 @@ export default async (req, res) => {
   const newsletterConfig = useRuntimeConfig().newsletter
   const providerName = Object.keys(newsletterConfig)[0];
 
-  try {
     const result = await fetch(
       `https://www.getrevue.co/api/v2/subscribers`,
       {
@@ -27,30 +26,9 @@ export default async (req, res) => {
 
     const jsonResponse = await result.json()
 
-    const message = jsonResponse.error ? jsonResponse : `Email ${jsonResponse.email} subscribed to ReVue`
+    if (result.status !== 200) throw createError({ statusCode: result.status, statusMessage: result.statusText })
 
-    res.statusCode = result.status
-    res.end(JSON.stringify(message))
-  } catch (error) {
-    res.statusCode = 500
-    res.end('Unexpected error occured', error)
-  }
-}
+    const message = jsonResponse.error ? jsonResponse : { message: `Email ${jsonResponse.email} subscribed to ReVue`, status: 200 }
 
-const subscribeToNewsletter = async (email: string, options: any) => {
-  const result = await fetch(
-    `https://www.getrevue.co/api/v2/subscribers`,
-    {
-      body: JSON.stringify({ email }),
-      headers: {
-        Authorization: `Token ${options.apiKey}`,
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    }
-  );
-
-  const jsonResponse = await result.json()
-
-  return { message: jsonResponse.error ? jsonResponse : `Email ${jsonResponse.email} subscribed to ReVue`, status: result.status }
-}
+    return message
+})
